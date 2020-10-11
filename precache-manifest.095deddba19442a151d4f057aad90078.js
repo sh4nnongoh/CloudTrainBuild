f7e11358c23fd252a3a4233f1b7fc6d17f1a0679
Shannon Goh <sh4nnongoh@gmail.com>
2020-10-11 22:43:38 +0800

    test api gateway

---
+++ precache-manifest.095deddba19442a151d4f057aad90078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d853d0665a1975c7cbf863e516607efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "558587fd44266c823af4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8b76497b0dcc9033fb52",
+    "url": "/static/css/main.010cee21.chunk.css"
+  },
+  {
+    "revision": "558587fd44266c823af4",
+    "url": "/static/js/2.c73f6a8b.chunk.js"
+  },
+  {
+    "revision": "107e9333deebcca0ffc0b99a703c53b6",
+    "url": "/static/js/2.c73f6a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b76497b0dcc9033fb52",
+    "url": "/static/js/main.8a2be9f3.chunk.js"
+  },
+  {
+    "revision": "7cabdd9fcb0ad8ecb446",
+    "url": "/static/js/runtime-main.7b34a03c.js"
+  }
+]);
